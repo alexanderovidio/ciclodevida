9c8bbcc23e759549439dc6e293330762cc92ddf0
Alexander <alexanderovidio@gmail.com>
2023-03-24 00:51:59 -0600

    ciclo de vida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ciclodevida';
+  title = 'ejercicio1';
+
+  nombreCliente = "Javier";
+  apellidoCliente = "Rodas";
+  direccionCliente = "Guatemala, Guatemala";
 }
